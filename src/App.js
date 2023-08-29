7f29ed1dc4df78dc4cfc0be411afffcc6e084052
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-30 00:49:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import Gallery from './Gallery'
+
+function App() {
+  return (
+  <>
+  <h1 className='mainH1'>Image Gallery</h1>
+  
+  </>
+
+  );
+}
+
+export default App;
