559a26f9bc59b291ae659c42f2240c0e01ac2115
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-30 18:02:45 +0200

    setModel FaWindow

--- src/Gallery.js
+++ src/Gallery.js
@@ -10,10 +10,17 @@ const [model,setModel] = useState(false);
 const [tempimgSrc,setTempImgSrc] = useState("")
 
 const getImg =(imgSrc) => {
-
+setTempImgSrc(imgSrc)
+setModel(true)
 }
 
   return (
+    <>
+    <div className={model ?  "model open" : "model"}>
+<img src={tempimgSrc}/>
+<FaWindowClose onClick={()=> setModel(false)} />
+    </div>
+
     <div className='gallery'>
         {Images.map((item,index)=>{
  return(
@@ -22,8 +29,8 @@ const getImg =(imgSrc) => {
     </div>
  )
 })}
-        
 </div>
+</>
   )
 }
 
